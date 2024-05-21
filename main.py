39b0de6cf8a07d59f5d9946aab6889077ffa9239
sosig132 <sosig132@gmail.com>
2024-05-21 17:16:36 +0300

    msg

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def tsp(points):
     dist_matrix = np.array(dist_matrix)
     dist_matrix[:, 0] = 0
 
-    # Matrice de distante (graf complet)
     print(dist_matrix)
 
 
@@ -37,7 +36,6 @@ def tsp(points):
 
 
 def main():
-    #size
 
     n = 10
     m = 2
@@ -64,7 +62,6 @@ def main():
 
     pts=optimal_path
 
-    # generate PATH so the vectors are pointing at each other
     PATH = []
     for i in range(len(pts) - 1):
         dx = pts[i+1][0] - pts[i][0]
@@ -73,28 +70,18 @@ def main():
         PATH.append((pts[i][0], pts[i][1], utils.rad2deg(theta)))
     PATH.append((pts[-1][0], pts[-1][1], 0))
 
-    # or you can also manually set the angles:
-    # PATH = [(-5,5,90),(-5,5,-90),(1,4,180), (5,4,0), (6,-3,90), (4,-4,-40),(-2,0,240), \
-    #         (-6, -7, 160), (-7,-1,80)]
 
-    # or just generate a random route:
-    # PATH = []
-    # for _ in range(10):
-    #     PATH.append((rd.randint(-7,7), rd.randint(-7,7), rd.randint(0,359)))
 
-    # init turtle
     tesla = turtle.Turtle()
-    tesla.speed(0) # 0: fast; 1: slow, 8.4: cool
+    tesla.speed(0) 
     tesla.shape('arrow')
     tesla.resizemode('user')
     tesla.shapesize(1, 1)
 
-    # draw vectors representing points in PATH
     for pt in PATH:
         draw.goto(tesla, pt)
         draw.vec(tesla)
 
-    # draw all routes found
     tesla.speed(0)
     for i in range(len(PATH) - 1):
         paths = rs.get_all_paths(PATH[i], PATH[i+1])
@@ -104,7 +91,6 @@ def main():
             draw.goto(tesla, PATH[i])
             draw.draw_path(tesla, path)
 
-    # draw shortest route
     tesla.pencolor(1, 0, 0)
     tesla.pensize(3)
     tesla.speed(10)
